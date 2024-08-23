485776b41925e0c36149b6c4a2fc98e2dce22c0b
Saad Imran <142585137+SaadIT23@users.noreply.github.com>
2024-08-23 22:29:29 +0500

    Update Photo.js

--- app/components/Photo.js
+++ app/components/Photo.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image'
 const Photo = () => {
     return (
-        <Image src="/saadbg.png" width={"500"} height={"500"} className='rounded-full  drop-shadow-lg shadow-lg animate-blink-shadow ' priority quality={100} alt='Saad'/>
+        <Image src="./saadbg.png" width={"500"} height={"500"} className='rounded-full  drop-shadow-lg shadow-lg animate-blink-shadow ' priority quality={100} alt='Saad'/>
     )
 }
 
