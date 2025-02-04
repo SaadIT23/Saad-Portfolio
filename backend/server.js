418b76fa29320170ffc00a10ce61547779de2177
Saad Imran <142585137+SaadIT23@users.noreply.github.com>
2025-02-04 14:21:07 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://saad-portfolio-2feq.vercel.app', 'https://saadit23.github.io/Saad-Portfolio/'],
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
