4340141cbc8f412f9dbd772cb7a50d83842a1096
Saad Imran <142585137+SaadIT23@users.noreply.github.com>
2025-02-04 14:44:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://saad-portfolio-2feq.vercel.app', 'https://saadit23.github.io/Saad-Portfolio/'],
+  origin: ['https://saad-portfolio-2feq.vercel.app', 'https://saadit23.github.io/Saad-Portfolio'],
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
